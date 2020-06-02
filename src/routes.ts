7e597f7841c593a06608a598e9bb272a7d2653e7
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-06-02 19:56:09 -0300

    arquivos staticos e listagem itens

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express';
+import knex from './database/conection';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ ola: 'mundo!' });
+routes.get('/items', async (req, res) => {
+  const itens = await knex('items').select('*');
+
+  const serializedItems = itens.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return res.json(serializedItems);
 });
 
 export default routes;
